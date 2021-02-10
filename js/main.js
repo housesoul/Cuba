f21031a82481ef457d7fed3386032fb16cb94a86
Evgheni <evgheni.cojocar@gmail.com>
2021-02-10 20:40:23 +0300

    3rd commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 
-$('.photo_slider').slick({
-    prevArrow: '<img class="slick-prev" src="images/arrow-left.jpg" alt="">',
-    nextArrow: '<img class="slick-next" src="images/arrow-right.jpg" alt="">',
-});
+    $('.photo_slider').slick({
+        prevArrow: '<img class="slick-prev" src="images/arrow-left.jpg" alt="">',
+        nextArrow: '<img class="slick-next" src="images/arrow-right.jpg" alt="">',
+    });
+
+    $('.menu_btn').on('click',function(){
+        $('.menu_list').slideToggle();
+    });
