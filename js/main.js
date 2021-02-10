ef2b37caf48dc4516c1ee7a23bb6ecddce76343c
Evgheni <evgheni.cojocar@gmail.com>
2021-02-10 14:36:31 +0300

    first_commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+$('.photo_slider').slick({
+    prevArrow: '<img class="slick-prev" src="images/arrow-left.jpg" alt="">',
+    nextArrow: '<img class="slick-next" src="images/arrow-right.jpg" alt="">',
+});
